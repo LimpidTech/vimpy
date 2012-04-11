41f1191e2f7240359217157ad8d377991ff0da05
Brandon R. Stoner <monokrome@LimpidTech.com>
2012-04-11 11:41:42 -0600

    Imported initial Pythonic settings wrapper.

---
+++ vimpy/conf.py
@@ -0,0 +1,32 @@
+import vim
+
+vim_setter_command = 'let {0}="{1}"'
+
+class VimpySettings(object):
+    """ Provides a dict-like interface which can be used to access variables.
+    
+    Overrides __getitem__ and __setitem__ in order to allow developers to
+    get and set the values of vimscript variables via a dict-like interface
+    using the vimpy.settings['variable_name']
+
+    """
+
+    # TODO: Verify that the proper prefixes are working.
+
+    def __getitem__(self, key):
+        """ Returns the value of a variable. """
+
+        return vim.eval(key)
+
+    def __setitem__(self, key, value):
+        """ Modifies the value of the provided variable. """
+
+        # Attempt to escape provided values to be safely provided to vim
+        final_value = '{0}'.format(value)
+        final_value = final_value.replace('\\', '\\\\')
+        final_value = final_value.replace('"', '\"')
+
+        vim.command(vim_setter_command.format(key, value))
+
+settings = VimpySettings()
+
