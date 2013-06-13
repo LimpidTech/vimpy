75450712567ad75b97451603cbeca97fb68a5bb0
Brandon R. Stoner <monokrome@limpidtech.com>
2013-06-12 22:30:42 -0700

    Made commands a bit easier to create! With magic!

--- vimpy/commands.py
+++ vimpy/commands.py
@@ -1,11 +1,16 @@
+# TODO: Consider making this functionality a decorator instead
+# of the __metaclass__ nightmare that it is.
+
 import vim
+from .util import AutoInstance
 
 command_register_template = 'command {name} python vimpy_core_commands_call("{name}")'
 command_unregister_template = 'delcommand {name}'
 
 def call_command(name):
-    if name in command_map:
-        command = command_map[name]
+    if name in global_command_map:
+        # TODO: Allow other command maps
+        command = global_command_map[name]
         command()
 
 class CommandMap(dict):
@@ -24,11 +29,16 @@ class CommandMap(dict):
 
         super(CommandMap, self).__delitem__(key)
 
+# A default global command map
+global_command_map = CommandMap()
+
 class Command(object):
     """ A command which can be used inside Vim. """
 
+    command_map = global_command_map
+    __metaclass__ = AutoInstance
+
     def __init__(self, command_map=None, register=True):
-        self.command_map = command_map
         name = getattr(self, 'name', False)
 
         if name is False:
@@ -40,16 +50,14 @@ class Command(object):
     def register(self):
         """ Registers the current command name with the Vim command map. """
 
-        command_map[self.name] = self
+        print(self.command_map)
+        self.command_map[self.name] = self
 
     def __call__(self, *args, **kwargs):
         self.run(*args, **kwargs)
 
     def run(self):
         raise NotImplementedError('Can not call base VimpyCommand.')
-
-# A default global command map
-command_map = CommandMap()
 
 # Wraps our commands in Python calls.
 vim.command("python from vimpy.commands import call_command as vimpy_core_commands_call")
